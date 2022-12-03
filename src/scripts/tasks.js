d4ffc90244893017867f2721e77133d4fbba0db3
Magn3tism <chapagainsakshyam@gmail.com>
2022-12-03 08:01:32 +0545

    Create class

---
+++ src/scripts/tasks.js
@@ -0,0 +1,8 @@
+export default class Task {
+  constructor(title, description, dueDate, prority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.prority = prority;
+  }
+}
